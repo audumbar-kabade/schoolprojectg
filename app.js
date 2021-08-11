d4ad806923294b489da4f8437ec63627699af316
AudumbarK <Audumbar.kabade@autoflowtech.com>
2021-08-11 21:19:05 +0530

    cron job

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cron = require('node-cron');
 
 var indexRouter = require('./routes/index');
-
-
 var app = express();
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+cron.schedule('* * * * *', () => {
+    console.log('running a task every minute');
+  });
 app.use('/', indexRouter);
 
 
